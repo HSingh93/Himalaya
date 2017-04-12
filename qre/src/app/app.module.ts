30636dab248b89fd4609eebeba2b2e28ef04ca26
DSnook <domenicksnook@gmail.com>
2017-04-11 22:24:43 -0400

    Added basic routing [DOM]

--- qre/src/app/app.module.ts
+++ qre/src/app/app.module.ts
@@ -2,21 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-
 import { AppComponent } from './app.component';
+
+import { NotFoundComponent } from './not-found.component';
+
+import { HomeComponent } from './components/home/home.component';
+import { CalculationComponent } from './components/calculation/calculation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CalculationComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'calculate',
+        component: CalculationComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ qre/src/app/not-found.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    template: `
+        <p>Sorry, could not find this route.</p>
+    `
+})
+export class NotFoundComponent { }
+
+
